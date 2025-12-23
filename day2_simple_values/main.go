4744318f74bacf533559f6c6db34456d6a8865d4
Anil Kumar <anilkumaar.akula@gmail.com>
2025-12-23 23:03:21 +0530

    practice day 1

---
+++ day2_simple_values/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	//sample values
+	// int
+	fmt.Println(1 + 1)
+	//bool
+	fmt.Println(true)
+	fmt.Println(false)
+	//string
+	fmt.Println("Anil kumar")
+	// float
+	fmt.Println(10.5)
+	fmt.Println(7.1 / 4.2)
+}
---
+++ day3_variable/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Anil kumar"
+	fmt.Println(name)
+
+	//infer
+	var Golang = "golang"
+	var isAdult bool = true
+
+	fmt.Println(Golang)
+	fmt.Println(isAdult)
+
+	var age int = 17
+	fmt.Println(age)
+	// shorthand syntex
+	name = "Ak47"
+	fmt.Println(name)
+	fmt.Printf("name-------->>>>%T\n", name)
+
+	var Something string
+	Something = "golang"
+	fmt.Println(Something)
+
+	var price float32 = 50.05
+	fmt.Println(price)
+
+	var prices = 5.05
+	fmt.Println(prices)
+
+	pricess := 10.09
+	fmt.Println(pricess)
+}
